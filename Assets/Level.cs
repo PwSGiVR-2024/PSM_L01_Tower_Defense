c8ad394f758cc10e077945e5143868bc6194d1df
gsaintskad <gsaintskad@gmail.com>
2025-06-08 17:47:41 +0200

    level classes init

---
+++ Assets/Level.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level{
+    public int level;
+    private List<MobWave> waves;
+}
---
+++ Assets/MobGroup.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MobGroup
+{
+    public GameObject MobPrefab;
+    public int MobQty;
+    public int spawnSpotId;
+
+}
