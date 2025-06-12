62851639a5f7ac58e9fdd123d929f2d5d00d1feb
gsaintskad <gsaintskad@gmail.com>
2025-06-12 06:59:04 +0200

    save commit

--- Assets/MobGroup.cs
+++ Assets/MobGroup.cs
@@ -3,8 +3,20 @@ using UnityEngine.Rendering;
 
 public class MobGroup
 {
-    public GameObject MobPrefab;
+    public GameObject MobPrefab; 
     public int MobQty;
     public int spawnSpotId;
+}
 
+public class TroyanMobGroup : MobGroup
+{
+    public TroyanMobGroup()
+    {
+        MobPrefab = Resources.Load<GameObject>("Prefab/TroyanEnemy");
+
+        if (MobPrefab == null)
+        {
+            Debug.LogError("Failed to load TroyanEnemy Variant prefab. Check the path and ensure it's in a Resources folder.");
+        }
+    }
 }
